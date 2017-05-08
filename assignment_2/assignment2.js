81ba4fd3753dc8c959c009f0d0f55b0fb7231a07
unknown <digitalhome@gmail.com>
2017-05-08 17:42:03 -0400

    Assignment2

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -3,6 +3,9 @@ Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
 
+for (var i = 1; i <=100; i++) {
+    console.log(i);
+}
 
 
 /*
@@ -17,3 +20,12 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+var i = 0;
+var pound = '#';
+while (i < 7) {
+    console.log(pound);
+    pound = pound + "#";
+    i = i + 1; 
+    
+}
